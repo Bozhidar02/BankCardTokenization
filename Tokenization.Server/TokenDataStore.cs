f66329b725911ec7d269b2c44a98b8d6b598968e
Bozhidar Goranov <goranovbobo@gmail.com>
2025-12-31 17:04:50 +0200

    Introduced thread safety to the file system

--- Tokenization.Server/TokenDataStore.cs
+++ Tokenization.Server/TokenDataStore.cs
@@ -7,31 +7,39 @@ namespace Tokenization.Server
 {
     public static class TokenDataStore
     {
+        private static readonly object _fileLock = new object();
         private const string FileName = "tokens.xml";
 
         public static Dictionary<string, string> LoadTokens()
         {
-            if (!File.Exists(FileName))
-                return new Dictionary<string, string>();
+            lock (_fileLock)
+            {
+                if (!File.Exists(FileName))
+                    return new Dictionary<string, string>();
 
-            FileStream fs = new FileStream(FileName, FileMode.Open);
-            var list = (List<TokenPair>)new XmlSerializer(
-                typeof(List<TokenPair>)).Deserialize(fs);
-
-            return list.ToDictionary(p => p.Token, p => p.CardNumber);
+                using (FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    var list = (List<TokenPair>)new XmlSerializer(typeof(List<TokenPair>)).Deserialize(fs);
+                    return list.ToDictionary(p => p.Token, p => p.CardNumber);
+                }
+            }
         }
 
         public static void SaveTokens(Dictionary<string, string> data)
         {
-            var list = data.Select(p => new TokenPair
+            lock (_fileLock)
             {
-                Token = p.Key,
-                CardNumber = p.Value
-            }).ToList();
+                var list = data.Select(p => new TokenPair
+                {
+                    Token = p.Key,
+                    CardNumber = p.Value
+                }).ToList();
 
-            FileStream fs = new FileStream(FileName, FileMode.Create);
-            new XmlSerializer(typeof(List<TokenPair>))
-                .Serialize(fs, list);
+                using (FileStream fs = new FileStream(FileName, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    new XmlSerializer(typeof(List<TokenPair>)).Serialize(fs, list);
+                }
+            }
         }
     }
 }
